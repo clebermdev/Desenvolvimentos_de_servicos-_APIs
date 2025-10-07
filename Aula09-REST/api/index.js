36cce1dcee6bedf94b7ae4b5f2c44d6efc910a08
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2025-10-07 11:00:47 -0300

    Aula09 - REST - CRUD de produto completo

--- Aula09-REST/api/index.js
+++ Aula09-REST/api/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const knex = require("knex")
-const erros = require("http-errors")
+const errors = require("http-errors")
 
 const app = express()
 
@@ -30,8 +30,73 @@ app.get( "/product"  , (req, res, next)=>{
         .catch( next )
 }  )
 
+app.get( "/product/last"  , (req, res, next)=>{
+    conn( "produto" )
+        .orderBy("id" , "desc")
+        .first()
+        .then( dados => res.json( dados ) )
+        .catch( next )
+}  )
 
 
+app.get( "/product/:idProd"  , (req, res, next)=>{
+    const id = req.params.idProd
+    conn( "produto" )
+        .where( "id" , id )
+        .first()
+        .then( dados => res.json( dados ) )
+        .catch( next )
+}  )
+
+
+app.post( "/product"  , (req, res, next)=>{
+    conn( "produto" )
+        .insert( req.body )
+        .then( dados => {
+            if( !dados ){
+                return next(errors( 404 , "Erro ao inserir") )
+            }
+            res.status(201).json( {
+                resposta : "Produto inserido" ,
+                id : dados[0]
+            } ) 
+        })
+        .catch( next )
+}  )
+
+
+app.put( "/product/:idProd"  , (req, res, next)=>{
+    const idProd = req.params.idProd
+    conn( "produto" )
+        .where( "id" , idProd )
+        .update( req.body )
+        .then( dados => {
+            if( !dados ){
+                return next(errors( 404 , "Erro ao editar") )
+            }
+            res.status(200).json( {
+                resposta : "Produto atualizado" 
+            } ) 
+        })
+        .catch( next )
+}  )
+
+
+app.delete( "/product/:idProd"  , (req, res, next)=>{
+    const id = req.params.idProd
+    conn( "produto" )
+        .where( "id" , id )
+        .delete()
+        .then( dados => {
+            if( !dados ){
+                return next(errors( 404 , "Erro ao tentar excluir") )
+            }
+            res.status(200).json( {
+                resposta : "Produto excluido" 
+            } ) 
+        } )
+        .catch( next )
+}  )
 
 app.listen( PORT , ()=>{
     console.log( `Loja executando em: http://${HOSTNAME}:${PORT}`)
