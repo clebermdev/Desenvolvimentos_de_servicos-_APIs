34b6707b9697bcb2c83bc1d9bb949b2a211d7cfb
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2025-10-14 09:14:54 -0300

    Aula09 - REST - Gabarito do exercício

--- Aula09-REST/api/index.js
+++ Aula09-REST/api/index.js
@@ -26,13 +26,17 @@ app.get( "/"  , (req, res, next)=>{
 
 app.get( "/product"  , (req, res, next)=>{
     conn( "produto" )
+        .leftJoin("categoria" , "produto.codCategoria", "=" , "categoria.id" )
+        .select( "produto.*" , "categoria.nome AS categoria")
         .then( dados => res.json( dados ) )
         .catch( next )
 }  )
 
 app.get( "/product/last"  , (req, res, next)=>{
     conn( "produto" )
-        .orderBy("id" , "desc")
+        .leftJoin("categoria" , "produto.codCategoria", "=" , "categoria.id" )
+        .select( "produto.*" , "categoria.nome AS categoria")
+        .orderBy("produto.id" , "desc")
         .first()
         .then( dados => res.json( dados ) )
         .catch( next )
@@ -42,7 +46,9 @@ app.get( "/product/last"  , (req, res, next)=>{
 app.get( "/product/:idProd"  , (req, res, next)=>{
     const id = req.params.idProd
     conn( "produto" )
-        .where( "id" , id )
+        .leftJoin("categoria" , "produto.codCategoria", "=" , "categoria.id" )
+        .select( "produto.*" , "categoria.nome AS categoria")
+        .where( "produto.id" , id )
         .first()
         .then( dados => res.json( dados ) )
         .catch( next )
@@ -93,6 +99,80 @@ app.delete( "/product/:idProd"  , (req, res, next)=>{
             }
             res.status(200).json( {
                 resposta : "Produto excluido" 
+            } ) 
+        } )
+        .catch( next )
+}  )
+
+app.get( "/category"  , (req, res, next)=>{
+    conn( "categoria" )
+        .then( dados => res.json( dados ) )
+        .catch( next )
+}  )
+
+app.get( "/category/last"  , (req, res, next)=>{
+    conn( "categoria" )
+        .orderBy("id" , "desc")
+        .first()
+        .then( dados => res.json( dados ) )
+        .catch( next )
+}  )
+
+
+app.get( "/category/:idCat"  , (req, res, next)=>{
+    const id = req.params.idCat
+    conn( "categoria" )
+        .where( "id" , id )
+        .first()
+        .then( dados => res.json( dados ) )
+        .catch( next )
+}  )
+
+
+app.post( "/category"  , (req, res, next)=>{
+    conn( "categoria" )
+        .insert( req.body )
+        .then( dados => {
+            if( !dados ){
+                return next(errors( 404 , "Erro ao inserir") )
+            }
+            res.status(201).json( {
+                resposta : "Categoria inserida" ,
+                id : dados[0]
+            } ) 
+        })
+        .catch( next )
+}  )
+
+
+app.put( "/category/:idCat"  , (req, res, next)=>{
+    const id = req.params.idCat
+    conn( "categoria" )
+        .where( "id" , id )
+        .update( req.body )
+        .then( dados => {
+            if( !dados ){
+                return next(errors( 404 , "Erro ao editar") )
+            }
+            res.status(200).json( {
+                resposta : "Categoria atualizada" 
+            } ) 
+        })
+        .catch( next )
+}  )
+
+
+app.delete( "/category/:idCat"  , (req, res, next)=>{
+    const id = req.params.idCat
+    conn( "categoria" )
+        .where( "id" , id )
+        .delete()
+        .then( dados => {
+            if( !dados ){
+                return next(errors( 404 , "Erro ao tentar excluir") )
+            }
+            res.status(200).json( {
+                resposta : "Categoria excluida" 
             } ) 
         } )
         .catch( next )
